aa675022646f796e81b0e81673eca6e1fcacbf16
Max-munene <munenemaxwell2@gmail.com>
2023-02-19 23:40:35 +0300

    carousel component

--- src/Components/Carousel.jsx
+++ src/Components/Carousel.jsx
@@ -1,1 +1,32 @@
 import React, { Component } from "react";
+import { Carousel } from "react-bootstrap";
+
+const Mycarousel = () => {
+  return (
+    <Carousel>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="./assets/img1.png"
+          alt="first slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>We shall put a small description of this slide here</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="/public/logo.png"
+          alt="Second slide"
+        />
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>We shall put a small description of this slide here</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+};
+export default Mycarousel;
--- src/home.jsx
+++ src/home.jsx
@@ -1,36 +1,14 @@
 import React, { Component } from "react";
 import Mynav from "./Components/Navbar";
-import { Carousel } from "react-bootstrap";
+import Mycarousel from "./Components/Carousel";
 
 const Home = () => {
   return (
     <div>
       <Mynav />
       <h1>This is the Home page</h1>
-      <Carousel>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="./assets/img1.png"
-            alt="first slide"
-          />
-          <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>We shall put a small description of this slide here</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="/public/logo.png"
-            alt="Second slide"
-          />
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>We shall put a small description of this slide here</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
+      <Mycarousel />
+
       <img src="/public/img1.png" alt="" />
     </div>
   );
